fd388bd49aea9a54c8c4ec2493a7dee206fd3d07
Zhyrgalbek <matisaev1414@gmail.com>
2021-11-14 21:01:37 +0300

    Создал папку store actions , reducers и написал логику redux

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -13,19 +13,22 @@ export const SET_FILTER = "SET_FILTER";
 // action creators
 
 export const add_todo = (text, date) => ({
-    type: ADD_TODO,
-    text,
-    date,
-})
+  type: ADD_TODO,
+  text,
+  date,
+});
+// delete
 export const delete_todo = (id) => ({
   type: DELETE_TODO,
   id,
 });
+// add
 export const edit_todo = (id, text) => ({
   type: EDIT_TODO,
   id: id,
   text: text,
 });
+// completed
 export const toggle_todo = (id, value) => ({
   type: TOGGLE_TODO,
   id,
@@ -35,5 +38,5 @@ export const toggle_todo = (id, value) => ({
 // filter
 export const set_filter = (filter) => ({
   type: SET_FILTER,
-    filter,
-})
+  filter,
+});
